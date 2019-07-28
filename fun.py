c5b46e42f5cff7ea5c3a423eee2364e5bd7f476a
aseel21-meet <aseel21@meet.mit.edu>
2019-07-28 11:31:14 -0400

    Stopping here

---
+++ fun.py
@@ -0,0 +1,18 @@
+total=0
+for number in range(1,10+1):
+    print(number)
+    total=total+number
+print(total)
+def add_numbers():
+    for numbers in range(1,11):
+        print(numbers)
+        total=numbers+10
+        if total==55:
+            print(total)
+            break
+answer=add_numbers()
+print(answer)
+
+
+    
+        
